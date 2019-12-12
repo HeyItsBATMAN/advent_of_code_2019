9255a005848144e9d39bf92cf0381484095e40eb
Kai Niebes <kai.niebes@outlook.com>
2019-12-12 07:59:03 +0100

    Day 12

---
+++ aoc12.cr
@@ -0,0 +1,102 @@
+require "benchmark"
+
+DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
+INPUT = File.read_lines("#{DAY}.txt")
+  .map(&.gsub("<", "").gsub(">", "").split(","))
+  .map(&.map(&.split("=")[1].to_i))
+
+class Moon
+  getter pos : Array(Int32)
+  getter vel : Array(Int32)
+  @orig : Array(Int32)
+
+  def initialize(pos : Array(Int32))
+    @pos, @vel = pos.dup, [0, 0, 0]
+    @orig = pos.dup
+  end
+
+  def is_orig(axis)
+    @pos[axis] == @orig[axis] && @vel[axis] == 0
+  end
+end
+
+def part1
+  moons = INPUT.dup.map { |moon| Moon.new(moon) }
+  (1..1000).each do |_|
+    moons.combinations(2).each { |pair| (0..2).each { |axis|
+      next if pair[0].pos[axis] == pair[1].pos[axis]
+      if pair[0].pos[axis] < pair[1].pos[axis]
+        pair[0].vel[axis] += 1
+        pair[1].vel[axis] -= 1
+      else
+        pair[0].vel[axis] -= 1
+        pair[1].vel[axis] += 1
+      end
+    } }
+
+    moons.each { |moon| (0..2).each { |axis|
+      moon.pos[axis] += moon.vel[axis]
+    } }
+  end
+
+  moons
+    .sum { |moon| moon.pos.map(&.abs).sum * moon.vel.map(&.abs).sum }
+end
+
+# https://en.wikipedia.org/wiki/Greatest_common_divisor#Binary_GCD_algorithm
+def gcd(u, v)
+  return u if (u == v)
+  return v if (u == 0)
+  return u if (v == 0)
+
+  if (u.even?)
+    return gcd(u >> 1, v) if (v.odd?)
+    return gcd(u >> 1, v >> 1) << 1
+  end
+
+  return gcd(u, v >> 1) if (v.even?)
+
+  return gcd((u - v) >> 1, v) if (u > v)
+  return gcd((v - u) >> 1, u)
+end
+
+# https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
+def lcm(u, v)
+  (u * v) // gcd(u, v)
+end
+
+def part2
+  moons = INPUT.dup.map { |moon| Moon.new(moon) }
+  origs = [0, 0, 0]
+  (0..UInt64::MAX).each do |step|
+    if step != 0
+      (0..2).each { |axis|
+        next if origs[axis] != 0
+        if moons.map { |moon| moon.is_orig(axis) }.select { |n| !n }.empty?
+          origs[axis] = step
+        end
+      }
+      break if origs.count(0) == 0
+    end
+
+    moons.combinations(2).each { |pair| (0..2).each { |axis|
+      next if pair[0].pos[axis] == pair[1].pos[axis]
+      if pair[0].pos[axis] < pair[1].pos[axis]
+        pair[0].vel[axis] += 1
+        pair[1].vel[axis] -= 1
+      else
+        pair[0].vel[axis] -= 1
+        pair[1].vel[axis] += 1
+      end
+    } }
+
+    moons.each { |moon| (0..2).each { |axis|
+      moon.pos[axis] += moon.vel[axis]
+    } }
+  end
+  origs = origs.map(&.to_u64)
+  lcm(origs[0], lcm(origs[1], origs[2]))
+end
+
+puts Benchmark.realtime { puts "Part 1 #{part1}" }.total_milliseconds
+puts Benchmark.realtime { puts "Part 2 #{part2}" }.total_milliseconds
